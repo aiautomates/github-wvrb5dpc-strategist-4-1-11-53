b527122c597fcd06d3251efcf25c333133f1b0cd
aiautomates <thedrivingtrafficformula@gmail.com>
2025-04-11 02:18:23 +0600

    Update Home.tsx
    youtube with 100%

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -487,7 +487,7 @@ function Home() {
 
             {/* Contact Form */}
             <div className="bg-gray-50 p-8 rounded-xl shadow-lg">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/ZM78-3uuPqg?si=eJO_LzVN92hKj2Gz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <iframe width="100%" height="315" src="https://www.youtube.com/embed/ZM78-3uuPqg?si=eJO_LzVN92hKj2Gz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </div>
           </div>
         </div>
